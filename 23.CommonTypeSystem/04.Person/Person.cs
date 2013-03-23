1e4b2f333dac7e4dcc912e7b5f024f52ae2da9eb
vlad0 <test@test.com>
2013-03-23 12:19:20 +0200

    adding 04.Person

---
+++ 23.CommonTypeSystem/04.Person/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Person
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int? Age { get; set; }
+
+        public Person() { }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Name: "+this.Name+"\n");
+
+            if (this.Age == null)
+            {
+                sb.Append("Age: N/A");
+            }
+            else
+            {
+                sb.Append("Age: "+this.Age+"");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
